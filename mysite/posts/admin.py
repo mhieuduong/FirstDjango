90bbf43a3e86d7785e3243f2d5824dc5beb8ec72
mhieu <mhieuduong@gmail.com>
2022-07-18 14:54:00 +0700

    admin django post

--- mysite/posts/admin.py
+++ mysite/posts/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-
 from .models import Post
 
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    date_hierarchy = 'updated_at'
+    list_display = ['title', 'created_at', 'updated_at']
+    ordering = ['title', 'created_at']
+    search_fields = ['title']
+
+
+admin.site.register(Post, PostAdmin)
